503c2f51dad4b298ce18f9f0d13e3e6666f42411
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-11-10 17:30:29 -0600

    connected s3 bucket to aws web service

--- server/create-bucket.js
+++ server/create-bucket.js
@@ -2,7 +2,8 @@
 // This package is responsible for the API that allows the application to communicate with the web service.
 const AWS = require('aws-sdk');
 
-// Set the region
+const { v4: uuidv4 } = require('uuid');
+
 // Set the region
 AWS.config.update({region: 'us-east-2'});
 
